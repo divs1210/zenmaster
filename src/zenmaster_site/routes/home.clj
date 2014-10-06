a1c2c98b7d5ef6c6e7893626b932a4a7c4c166da
Divyansh Prakash <divyansh.prakash@yahoo.com>
2014-10-06 17:38:19 +0530

    First commit.

---
+++ src/zenmaster_site/routes/home.clj
@@ -0,0 +1,15 @@
+(ns zenmaster-site.routes.home
+  (:require [compojure.core :refer :all]
+            [zenmaster-site.layout :as layout]
+            [zenmaster-site.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
